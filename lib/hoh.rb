fabfd01c1096b7c33a8de957be72da0652943d99
Kat Maldon <katharinamaldon@gmail.com>
2020-03-28 22:55:03 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -4,15 +4,8 @@
 #
 # The tests will guide your construction.
 #
-# PORTION_1 = {
-#   label: "Kingdom",
-#   sub_category: {
-#     label: "Phylum",
-#     sub_category: {
-#       label: "Class"
-#     }
-#   }
-# }
+
+
 # 
 # PORTION_2 = {
 #   label: "Order"
