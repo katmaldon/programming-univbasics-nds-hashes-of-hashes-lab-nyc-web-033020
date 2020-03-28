1e2bc66cb555978ca35ab49560c7c127b5a5d710
Kat Maldon <katharinamaldon@gmail.com>
2020-03-28 23:14:07 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -1,29 +1,6 @@
-# hoh = {
-#   label: Kingdom,
-#   sub_category: {
-#     label: Phylum,
-#     sub_category: {
-#       label: Class,
-#       sub_category: {
-#         label: Order,
-#         sub_category: {
-#           label: Family,
-#           sub_category: {
-#             label: Genus, 
-#             sub_category: {
-#               label: Species,
-#               sub_category: nil 
-#             }
-#           }
-#         }
-#       }
-#     }
-#   }
-# }
- 
 def naming_system
   
-starting_node = {
+hoh = {
     label: "Kingdom",
    sub_category: {
      label: "Phylum",
